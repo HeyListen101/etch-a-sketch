bbb955a845e5eadff87a256d721bcf95731582da
jose <josematienzo101@gmail.com>
2023-08-17 16:33:34 +0800

    Add dynamic size functionality of grid

--- script.js
+++ script.js
@@ -5,21 +5,41 @@
 // Column divs must flex horizontally
 
 const container = document.querySelector('.container');
+const containerWidth = container.clientWidth;
+const containerHeight = container.clientHeight;
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.classList.add(`row${i}`);
-    row.style.display = 'flex';
-    row.style.margin = 0;
-    row.style.width = '640px';
-    row.style.height = '40px';
+function createGrid(numOfSquares) {
+    container.innerHTML = "";
+    for (let i = 0; i < numOfSquares; i++) {
+        const row = document.createElement('div');
+        row.classList.add(`row${i}`);
+        row.style.display = 'flex';
+        row.style.margin = 0;
+        row.style.width = `${containerWidth}px`;
+        row.style.height = `${containerHeight / numOfSquares}px`;
 
-    for (let j = 0; j < 16; j++) {
-        const col = document.createElement('div');
-        col.style.width = '40px';
-        col.style.height = '40px';
-        row.appendChild(col);
+        for (let j = 0; j < numOfSquares; j++) {
+            const col = document.createElement('div');
+            col.style.width = `${containerWidth / numOfSquares}px`;
+            col.style.height = `${containerHeight / numOfSquares}px`;
+            col.classList.add('cell');
+            row.appendChild(col);
+        }
+
+        container.appendChild(row);
     }
-
-    container.appendChild(row);
 }
+
+createGrid(16);
+
+const setSzBtn = document.querySelector('.setSize');
+
+setSzBtn.addEventListener('click', () => {
+    let size = 0;
+    do {
+        size = prompt('Enter Size (n) of Grid (n x n): ');
+    } while (size > 100);
+
+    createGrid(size);
+});
+
