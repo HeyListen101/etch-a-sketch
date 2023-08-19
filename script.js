7f28bf7be5d7bfa3679314f58731eb2ba9a931db
jose <josematienzo101@gmail.com>
2023-08-19 16:11:35 +0800

    Add darken color option

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const slider = document.querySelector('.slider');
 const sizeVal = document.querySelector('.grid-size');
 const picker = document.querySelector('.current-color');
 const clear = document.querySelector('.clear');
+const dark = document.querySelector('.darken');
 const containerWidth = container.clientWidth;
 const containerHeight = container.clientHeight;
 let color = 'black';
 let mouseDown = false;
+let option = 'color';
 createGrid(slider.value);
 
 document.body.addEventListener('mousedown', () => {
@@ -15,6 +17,13 @@ document.body.addEventListener('mousedown', () => {
 document.body.addEventListener('mouseup', () => {
     mouseDown = false;
 });
+
+// Darkening by 10%
+dark.addEventListener('click', () => {
+    rgb = hexToRgb(color);
+    option = 'dark';
+});
+
 
 // Color picker option for users
 picker.addEventListener('input', () => {
@@ -48,11 +57,26 @@ function createGrid(numOfSquares) {
             col.style.height = `${containerHeight / numOfSquares}px`;
             
             col.addEventListener('mousedown', () => {
-                col.style.backgroundColor = color;
+                if (option == 'color') {
+                    col.style.backgroundColor = color;
+                } else if (option == 'dark') {
+                    rgb = hexToRgb(color);
+                    color = `rgb(${rgb.r * 0.9}, ${rgb.g * 0.9}, ${rgb.b * 0.9})`;
+                    col.style.backgroundColor = color;
+                    color = rgbToHex(rgb.r * 0.9, rgb.g * 0.9, rgb.b * 0.9);
+                }
             });
             col.addEventListener('mouseover', () => {
-                if (mouseDown)
-                    col.style.backgroundColor = color;
+                if (mouseDown) { 
+                    if (option == 'color') {
+                        col.style.backgroundColor = color;
+                    } else if (option == 'dark') {
+                        rgb = hexToRgb(color);
+                        color = `rgb(${rgb.r * 0.9}, ${rgb.g * 0.9}, ${rgb.b * 0.9})`;
+                        col.style.backgroundColor = color;
+                        color = rgbToHex(rgb.r * 0.9, rgb.g * 0.9, rgb.b * 0.9);
+                    }
+                }
             });
             
             col.classList.add('cell');
@@ -61,3 +85,14 @@ function createGrid(numOfSquares) {
         container.appendChild(row);
     }
 }
+
+function hexToRgb(hex) {
+    const r = parseInt(hex.slice(1, 3), 16);
+    const g = parseInt(hex.slice(3, 5), 16);
+    const b = parseInt(hex.slice(5, 7), 16);
+    return { r, g, b };
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1);
+  }
