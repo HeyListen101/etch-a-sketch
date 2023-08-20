434111fcb8cf33a5a7922adc60dd119421feb1ae
jose <josematienzo101@gmail.com>
2023-08-20 15:19:37 +0800

    Optimize scripting

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const container = document.querySelector('.container');
+const colorMode = document.querySelector('.color-mode');
+const dark = document.querySelector('.darken');
+const rainbow = document.querySelector('.rainbow');
+const clear = document.querySelector('.clear');
 const slider = document.querySelector('.slider');
 const sizeVal = document.querySelector('.grid-size');
 const picker = document.querySelector('.current-color');
-const clear = document.querySelector('.clear');
-const dark = document.querySelector('.darken');
-const rainbow = document.querySelector('.rainbow');
-const colorMode = document.querySelector('.color-mode');
 const containerWidth = container.clientWidth;
 const containerHeight = container.clientHeight;
+let option = 'color';
 let color = 'black';
 let mouseDown = false;
-let option = 'color';
 createGrid(slider.value);
 
 document.body.addEventListener('mousedown', () => {
@@ -57,9 +57,7 @@ function createGrid(numOfSquares) {
     container.innerHTML = "";
     for (let i = 0; i < numOfSquares; i++) {
         const row = document.createElement('div');
-        row.classList.add(`row${i}`);
-        row.style.display = 'flex';
-        row.style.margin = 0;
+        row.classList.add('row');
         row.style.width = `${containerWidth}px`;
         row.style.height = `${containerHeight / numOfSquares}px`;
 
@@ -69,37 +67,11 @@ function createGrid(numOfSquares) {
             col.style.height = `${containerHeight / numOfSquares}px`;
             
             col.addEventListener('mousedown', () => {
-                if (option == 'color') {
-                    col.style.backgroundColor = color;
-                } else if (option == 'dark') {
-                    rgb = hexToRgb(color);
-                    color = `rgb(${rgb.r * 0.9}, ${rgb.g * 0.9}, ${rgb.b * 0.9})`;
-                    col.style.backgroundColor = color;
-                    color = rgbToHex(rgb.r * 0.9, rgb.g * 0.9, rgb.b * 0.9);
-                } else if (option == 'rainbow') {
-                    rVal = Math.floor(Math.random() * 255);
-                    gVal = Math.floor(Math.random() * 255);
-                    bVal = Math.floor(Math.random() * 255);
-                    color = rgbToHex(rVal, gVal, bVal);
-                    col.style.backgroundColor = color;
-                }
+                changeColor(col);
             });
             col.addEventListener('mouseover', () => {
                 if (mouseDown) { 
-                    if (option == 'color') {
-                        col.style.backgroundColor = color;
-                    } else if (option == 'dark') {
-                        rgb = hexToRgb(color);
-                        color = `rgb(${rgb.r * 0.9}, ${rgb.g * 0.9}, ${rgb.b * 0.9})`;
-                        col.style.backgroundColor = color;
-                        color = rgbToHex(rgb.r * 0.9, rgb.g * 0.9, rgb.b * 0.9);
-                    } else if (option == 'rainbow') {
-                        rVal = Math.floor(Math.random() * 255);
-                        gVal = Math.floor(Math.random() * 255);
-                        bVal = Math.floor(Math.random() * 255);
-                        color = rgbToHex(rVal, gVal, bVal);
-                        col.style.backgroundColor = color;
-                    }
+                    changeColor(col);
                 }
             });
             
@@ -108,6 +80,19 @@ function createGrid(numOfSquares) {
         }
         container.appendChild(row);
     }
+}
+
+function changeColor(col) {
+    if (option == 'dark') {
+        rgb = hexToRgb(color);
+        color = rgbToHex(rgb.r * 0.9, rgb.g * 0.9, rgb.b * 0.9);
+    } else if (option == 'rainbow') {
+        rVal = Math.floor(Math.random() * 255);
+        gVal = Math.floor(Math.random() * 255);
+        bVal = Math.floor(Math.random() * 255);
+        color = rgbToHex(rVal, gVal, bVal);
+    }
+    col.style.backgroundColor = color;
 }
 
 function hexToRgb(hex) {
@@ -119,4 +104,4 @@ function hexToRgb(hex) {
 
 function rgbToHex(r, g, b) {
     return "#" + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1);
-  }
+}
